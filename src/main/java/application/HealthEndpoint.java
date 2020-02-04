f4d95c003d416e6cbb299fd31e68832bd4fad758
Chuck Cox <chuckc@us.ibm.com>
2020-02-04 14:12:11 -0600

    test: status

--- src/main/java/application/HealthEndpoint.java
+++ src/main/java/application/HealthEndpoint.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 //import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Rxsponse;
+import javax.ws.rs.core.Response;
 
 @Path("health")
 public class HealthEndpoint {
@@ -18,7 +18,7 @@ public class HealthEndpoint {
         return Response.status(503).entity("{\"status\":\"DOWN\"}").build();
       }
       */
-      return Response.ok("{\"status\":\"DOWN\"}").build();
+      return Response.status(401).build();
     }
 
 }
